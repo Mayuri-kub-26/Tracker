9dea9d4257644cfe0367c2064a27879fb3cac461
Mayuri-kub-26 <mmayu8497@gmail.com>
2026-01-29 15:56:10 +0530

    Update version

--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ def read_version():
 
 print("=================================")
 print("Running Tracker Version:", read_version())
-print("A new updated version (v6.6.0)")
+print("As it is a new version all the upgraded versions will be seen in this version")
 print("=================================")
 
 
